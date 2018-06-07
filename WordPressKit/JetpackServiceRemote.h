dc760c3a0497cdf7d811a663427a6343128bdac2
Sergio Estevao <sergioestevao@gmail.com>
2018-06-07 12:54:37 +0100

    Comment out code.

--- WordPressKit/JetpackServiceRemote.h
+++ WordPressKit/JetpackServiceRemote.h
@@ -4,25 +4,25 @@
 /**
  * Error returned as the domain to NSError from JetpackServiceRemote.
  */
-extern NSString * const JetpackServiceRemoteErrorDomain;
+//extern NSString * const JetpackServiceRemoteErrorDomain;
 
 /**
  * Possible NSError codes for BlogJetpackErrorDomain.
  */
-typedef NS_ENUM(NSInteger, JetpackServiceRemoteErrorCode) {
-    // The user doesn't have access to that specific blog
-    JetpackServiceRemoteErrorNoRecordForBlog,
-    // The provided username/password are invalid
-    JetpackServiceRemoteErrorInvalidCredentials,
-};
+//typedef NS_ENUM(NSInteger, JetpackServiceRemoteErrorCode) {
+//    // The user doesn't have access to that specific blog
+//    JetpackServiceRemoteErrorNoRecordForBlog,
+//    // The provided username/password are invalid
+//    JetpackServiceRemoteErrorInvalidCredentials,
+//};
 
 @interface JetpackServiceRemote : ServiceRemoteWordPressComREST
 
-- (void)validateJetpackUsername:(NSString *)username
-                       password:(NSString *)password
-                      forSiteID:(NSNumber *)siteID
-                        success:(void (^)(NSArray *blogIDs))success
-                        failure:(void (^)(NSError *error))failure;
+//- (void)validateJetpackUsername:(NSString *)username
+//                       password:(NSString *)password
+//                      forSiteID:(NSNumber *)siteID
+//                        success:(void (^)(NSArray *blogIDs))success
+//                        failure:(void (^)(NSError *error))failure;
 
 - (void)checkSiteHasJetpack:(NSURL *)siteURL
                     success:(void (^)(BOOL hasJetpack))success
