ba8d6c75e9d7088a1991f8fe44dea446d3b0f20c
Jan Klausa <jan@klausa.pl>
2018-08-13 01:27:17 +0200

    Add AutomatedTransferService.

---
+++ WordPressKit/AutomatedTransferStatus.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+/// A helper object encapsulating a status of Automated Transfer operation.
+public struct AutomatedTransferStatus {
+    public enum State: String, RawRepresentable {
+        case complete
+        case pending
+        case active
+    }
+
+    let status: State
+    let step: Int?
+    let totalSteps: Int?
+
+    init?(status statusString: String) {
+        guard let status = State(rawValue: statusString) else {
+            return nil
+        }
+
+        self.status = status
+        self.step = nil
+        self.totalSteps = nil
+    }
+
+    init?(status statusString: String, step: Int, totalSteps: Int) {
+        guard let status = State(rawValue: statusString) else {
+            return nil
+        }
+
+        self.status = status
+        self.step = step
+        self.totalSteps = totalSteps
+    }
+
+}
