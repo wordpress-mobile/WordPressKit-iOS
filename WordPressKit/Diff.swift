90d220aa8aea300c0852d72b8cae413ec52f9fa5
Daniele Bogo <daniele.bogo@automattic.com>
2018-10-29 11:12:04 +0000

    Add documentation

--- WordPressKit/Diff.swift
+++ WordPressKit/Diff.swift
@@ -3,16 +3,24 @@ import Foundation
 
 /// Diff model
 public struct Diff: Codable {
+    /// Revision id from the content has been changed
     public var fromRevisionId: Int
+
+    /// Current revision id
     public var toRevisionId: Int
 
+    /// Model for the diff values
     public var values: DiffValues
 
+    /// Mapping keys
     private enum CodingKeys: String, CodingKey {
         case fromRevisionId = "from"
         case toRevisionId = "to"
         case values = "diff"
     }
+
+
+    // MARK - Decode protocol
 
     public init(from decoder: Decoder) throws {
         let data = try decoder.container(keyedBy: CodingKeys.self)
@@ -25,11 +33,16 @@ public struct Diff: Codable {
 
 
 public struct DiffValues: Codable {
+    /// Model for the diff totals operations
     public var totals: DiffTotals?
 
+    /// Title diffs array
     public var titleDiffs: [DiffValue]
+
+    /// Content diffs array
     public var contentDiffs: [DiffValue]
 
+    /// Mapping keys
     private enum CodingKeys: String, CodingKey {
         case titleDiffs = "post_title"
         case contentDiffs = "post_content"
@@ -38,15 +51,22 @@ public struct DiffValues: Codable {
 }
 
 
+/// DiffTotals model
 public struct DiffTotals: Codable {
+    /// Total of additional operations
     public var totalAdditions: Int
+
+    /// Total of deletions operations
     public var totalDeletions: Int
 
+    /// Mapping keys
     private enum CodingKeys: String, CodingKey {
         case totalAdditions = "add"
         case totalDeletions = "del"
     }
 
+
+    // MARK - Decode protocol
 
     public init(from decoder: Decoder) throws {
         let data = try decoder.container(keyedBy: CodingKeys.self)
@@ -57,6 +77,12 @@ public struct DiffTotals: Codable {
 }
 
 
+/// Diff Operation
+///
+/// - add: Addition
+/// - copy: Copy
+/// - del: Deletion
+/// - unknown: Default value
 public enum DiffOperation: String, Codable {
     case add
     case copy
@@ -65,14 +91,22 @@ public enum DiffOperation: String, Codable {
 }
 
 
+/// DiffValue
 public struct DiffValue: Codable {
+    /// Diff operation
     public var operation: DiffOperation
+
+    /// Diff value
     public var value: String?
 
+    /// Mapping keys
     private enum CodingKeys: String, CodingKey {
         case operation = "op"
         case value
     }
+
+
+    // MARK - Decode protocol
 
     public init(from decoder: Decoder) throws {
         let data = try decoder.container(keyedBy: CodingKeys.self)
