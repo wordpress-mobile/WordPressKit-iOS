eede9776504281d3787503087c338f97c8f39097
Jorge Bernal <jbernal@gmail.com>
2019-03-11 21:40:49 +0100

    Add callback property to handle invalid tokens

--- WordPressKit/WordPressComRestApi.swift
+++ WordPressKit/WordPressComRestApi.swift
@@ -60,6 +60,8 @@ open class WordPressComRestApi: NSObject {
 
     private let localeKey: String
 
+    private var invalidTokenHandler: (() -> Void)? = nil
+
     /**
      Configure whether or not the user's preferred language locale should be appended. Defaults to true.
      */
@@ -152,6 +154,14 @@ open class WordPressComRestApi: NSObject {
     @objc open func invalidateAndCancelTasks() {
         sessionManager.session.invalidateAndCancel()
         uploadSessionManager.session.invalidateAndCancel()
+    }
+
+    public func authTokenEquals(token: String) -> Bool {
+        return token == oAuthToken
+    }
+
+    @objc func setInvalidTokenHandler(_ handler: @escaping () -> Void) {
+        invalidTokenHandler = handler
     }
 
     // MARK: Network requests
@@ -443,6 +453,9 @@ extension WordPressComRestApi {
         ]
 
         let mappedError = errorsMap[errorCode] ?? WordPressComRestApiError.unknown
+        if mappedError == .invalidToken {
+            invalidTokenHandler?()
+        }
         userInfo[WordPressComRestApi.ErrorKeyErrorCode] = errorCode
         userInfo[WordPressComRestApi.ErrorKeyErrorMessage] = errorDescription
         userInfo[NSLocalizedDescriptionKey] =  errorDescription
