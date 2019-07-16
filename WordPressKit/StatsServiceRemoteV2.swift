64f6552a078b7cb3bb43355a8de1c83d12b66fba
Stephenie Harris <scoutharris@gmail.com>
2019-07-16 10:05:45 -0600

    Use site timezone for Period stats.

--- WordPressKit/StatsServiceRemoteV2.swift
+++ WordPressKit/StatsServiceRemoteV2.swift
@@ -15,7 +15,7 @@ public class StatsServiceRemoteV2: ServiceRemoteWordPressComREST {
 
     private lazy var periodDataQueryDateFormatter: DateFormatter = {
         let df = DateFormatter()
-        df.locale = Locale(identifier: "en_US_POSIX")
+        df.timeZone = siteTimezone
         df.dateFormat = "yyyy-MM-dd"
         return df
     }()
