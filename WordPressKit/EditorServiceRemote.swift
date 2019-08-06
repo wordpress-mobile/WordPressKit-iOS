b6e7149071d8fae2dcf9418844154f1931ba3144
etoledom <etoledom@icloud.com>
2019-08-06 09:56:24 +0300

    EditorServiceRemote: Adding `me/gutenberg` post request.
    This endpoint will set the specified mobile editor preference to all user's sites

--- WordPressKit/EditorServiceRemote.swift
+++ WordPressKit/EditorServiceRemote.swift
@@ -18,6 +18,22 @@ public class EditorServiceRemote: ServiceRemoteWordPressComREST {
         }
     }
 
+    public func postDesignateMobileEditorForAllSites(_ editor: EditorSettings.Mobile, success: @escaping () -> Void, failure: @escaping (Error) -> Void) {
+        let endpoint = "me/gutenberg"
+        let path = self.path(forEndpoint: endpoint, withVersion: ._2_0)
+
+        let parameters = [
+            "platform": "mobile",
+            "editor": editor.rawValue,
+        ] as [String: AnyObject]
+
+        wordPressComRestApi.POST(path, parameters: parameters, success: { (responseObject, httpResponse) in
+            success()
+        }) { (error, httpError) in
+            failure(error)
+        }
+    }
+
     public func getEditorSettings(_ siteID: Int, success: @escaping (EditorSettings) -> Void, failure: @escaping (Error) -> Void) {
         let endpoint = "sites/\(siteID)/gutenberg"
         let path = self.path(forEndpoint: endpoint, withVersion: ._2_0)
