7ff68231765d7ffa1cb24bf704c3babf2e818020
Stephenie Harris <scoutharris@gmail.com>
2019-08-21 18:03:54 -0600

    Adding apple specific parameters to social service connect.

--- WordPressKit/AccountServiceRemoteREST+SocialService.swift
+++ WordPressKit/AccountServiceRemoteREST+SocialService.swift
@@ -13,19 +13,35 @@ extension AccountServiceRemoteREST {
     /// - Parameters:
     ///     - service The name of the social service.
     ///     - token The OpenID Connect (JWT) ID token identifying the user on the social service.
+    ///     - appleEmail The email address associated with an Apple ID. Optional.
+    ///     - appleFullName The full name associated with an Apple ID. Optional.
     ///     - oAuthClientID The WPCOM REST API client ID.
     ///     - oAuthClientSecret The WPCOM REST API client secret.
     ///     - success The block that will be executed on success.
     ///     - failure The block that will be executed on failure.
-    public func connectToSocialService(_ service: SocialServiceName, serviceIDToken token: String, oAuthClientID: String, oAuthClientSecret: String, success:@escaping (() -> Void), failure:@escaping ((NSError) -> Void)) {
+    public func connectToSocialService(_ service: SocialServiceName,
+                                       serviceIDToken token: String,
+                                       appleEmail: String? = nil,
+                                       appleFullName: String? = nil,
+                                       oAuthClientID: String,
+                                       oAuthClientSecret: String,
+                                       success:@escaping (() -> Void),
+                                       failure:@escaping ((NSError) -> Void)) {
         let path = self.path(forEndpoint: "me/social-login/connect", withVersion: ._1_1)
 
-        let params = [
+        var params = [
             "client_id": oAuthClientID,
             "client_secret": oAuthClientSecret,
             "service": service.rawValue,
             "id_token": token,
         ] as [String: AnyObject]
+        
+        // Append Apple specific parameters
+        if service == .apple {
+            params["user_email"] = appleEmail as AnyObject
+            params["user_name"] = appleFullName as AnyObject
+        }
+        
         wordPressComRestApi.POST(path, parameters: params, success: { (responseObject, httpResponse) in
             success()
         }, failure: { (error, httpResponse) in
