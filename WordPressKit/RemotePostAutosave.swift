85bb8a50c6ffd6de403be4485231b589f03694ce
Paul Von Schrottky <paulvonschrottky@gmail.com>
2019-11-02 14:41:42 -0300

    Make RemotePostAutosave Obj-C compatible

--- WordPressKit/RemotePostAutosave.swift
+++ WordPressKit/RemotePostAutosave.swift
@@ -1,13 +1,15 @@
 import Foundation
 
 /// Encapsulates the autosave attributes of a post.
-@objcMembers class RemotePostAutosave: NSObject {
-    var title: String?
-    var excerpt: String?
-    var content: String?
-    var modifiedDate: Date?
-    var identifier: NSNumber?
-    var authorID: String?
-    var postID: NSNumber?
-    var previewURL: String?
+@objc
+@objcMembers
+open class RemotePostAutosave: NSObject {
+    open var title: String?
+    open var excerpt: String?
+    open var content: String?
+    open var modifiedDate: Date?
+    open var identifier: NSNumber?
+    open var authorID: String?
+    open var postID: NSNumber?
+    open var previewURL: String?
 }
