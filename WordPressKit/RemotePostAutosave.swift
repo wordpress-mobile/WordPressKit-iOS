bfb8412e36db12da2e4b7f87a9662a90f49fc104
Paul Von Schrottky <paulvonschrottky@gmail.com>
2019-11-05 21:22:08 -0300

    Tighten access control on RemotePostAutosave
    `open` allowed subclassing of RemotePostAutosave and overriding of its
    properties, `public` disallows this. Further restiction to disallow
    setting the properties from outside the module using internal(set)
    doesn't work due to Obj-C interoperability issues.

--- WordPressKit/RemotePostAutosave.swift
+++ WordPressKit/RemotePostAutosave.swift
@@ -3,13 +3,13 @@ import Foundation
 /// Encapsulates the autosave attributes of a post.
 @objc
 @objcMembers
-open class RemotePostAutosave: NSObject {
-    open var title: String?
-    open var excerpt: String?
-    open var content: String?
-    open var modifiedDate: Date?
-    open var identifier: NSNumber?
-    open var authorID: String?
-    open var postID: NSNumber?
-    open var previewURL: String?
+public class RemotePostAutosave: NSObject {
+    public var title: String?
+    public var excerpt: String?
+    public var content: String?
+    public var modifiedDate: Date?
+    public var identifier: NSNumber?
+    public var authorID: String?
+    public var postID: NSNumber?
+    public var previewURL: String?
 }
