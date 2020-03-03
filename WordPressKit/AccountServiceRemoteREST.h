36f28b0d7d6cce66044333acb626583786320956
Diego Rey Mendez <diegoreymendez@gmail.com>
2020-03-03 11:25:28 -0300

    Improves the error handling in our email checking service.

--- WordPressKit/AccountServiceRemoteREST.h
+++ WordPressKit/AccountServiceRemoteREST.h
@@ -2,6 +2,14 @@
 #import "AccountServiceRemote.h"
 #import "ServiceRemoteWordPressComREST.h"
 
+static NSString * const AccountServiceRemoteErrorDomain = @"AccountServiceErrorDomain";
+
+typedef NS_ERROR_ENUM(AccountServiceRemoteErrorDomain, AccountServiceRemoteError) {
+    AccountServiceRemoteCantReadServerResponse,
+    AccountServiceRemoteEmailAddressInvalid,
+    AccountServiceRemoteEmailAddressTaken,
+};
+
 @interface AccountServiceRemoteREST : ServiceRemoteWordPressComREST <AccountServiceRemote>
 
 @end
