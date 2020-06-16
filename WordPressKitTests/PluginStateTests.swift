a893819d50d60101f0574d2e9217b88a88d13e26
Charlie <scheersound@gmail.com>
2020-06-16 20:00:49 +1200

    created setup for PluginStateTests

--- WordPressKitTests/PluginStateTests.swift
+++ WordPressKitTests/PluginStateTests.swift
@@ -3,7 +3,21 @@ import WordPressKit
 
 class PluginStateTests: XCTestCase {
     
-    func testPluginStateEquatableSucceeds() {
+    var decoder: JSONDecoder!
+    var remote: PluginServiceRemote!
+    
+    override func setUp() {
+        super.setUp()
         
+        decoder = JSONDecoder()
+        remote = PluginServiceRemote()
     }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        decoder = nil
+        remote = nil
+    }
+
 }
