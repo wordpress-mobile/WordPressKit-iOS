482104a4de6dc9065f446c803910b5adce09c7e3
Charlie <scheersound@gmail.com>
2020-06-26 21:50:13 +1200

    Created base for AtomicTransferTests to test the AtomicTransferServiceRemote

---
+++ WordPressKitTests/AutomatedTransferTests.swift
@@ -0,0 +1,9 @@
+//
+//  AutomatedTransferTests.swift
+//  WordPressKitTests
+//
+//  Created by Charlie Scheer on 6/26/20.
+//  Copyright © 2020 Automattic Inc. All rights reserved.
+//
+
+import Foundation
