c08dfae2c6c53fbaf783b9b620aa5ad0a499c7de
Charlie <scheersound@gmail.com>
2020-06-30 18:46:44 +1200

    Revert "Created base for AtomicTransferTests to test the AtomicTransferServiceRemote"
    This reverts commit 482104a4de6dc9065f446c803910b5adce09c7e3.

--- WordPressKitTests/AutomatedTransferTests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AutomatedTransferTests.swift
-//  WordPressKitTests
-//
-//  Created by Charlie Scheer on 6/26/20.
-//  Copyright © 2020 Automattic Inc. All rights reserved.
-//
-
-import Foundation
