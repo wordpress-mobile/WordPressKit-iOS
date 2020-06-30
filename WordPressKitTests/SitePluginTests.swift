474b09d82a5a86d03b234c61cd34538de41839f8
Charlie <scheersound@gmail.com>
2020-06-30 20:32:36 +1200

    Changed SitePluginTests to use locally defined variables instead of creating them from JSON data

--- WordPressKitTests/SitePluginTests.swift
+++ WordPressKitTests/SitePluginTests.swift
@@ -3,40 +3,19 @@ import XCTest
 @testable import WordPressKit
 
 class SitePluginTests: RemoteTestCase, RESTTestable {
-    let siteID = 123
-    
-    var sitePluginsEndpoint: String {
-        return "sites/\(siteID)/plugins"
-    }
-    
-    var sitePluginCapabilitiesA: SitePluginCapabilities!
-    
-    override func setUp() {
-        super.setUp()
-        
-        let remote = PluginServiceRemote(wordPressComRestApi: getRestApi())
-        let sitePluginsMockPath = Bundle(for: type(of: self)).path(forResource: "site-plugins-success", ofType: "json")!
-        let json = JSONLoader().loadFile(sitePluginsMockPath) as AnyObject
-        guard let response = json as? [String : AnyObject] else {
-            return
-        }
-        sitePluginCapabilitiesA = try! remote.pluginCapabilities(response: response)
-        sitePluginCapabilitiesA = try! remote.pluginCapabilities(response: response)
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
     func testSitePluginCapabilitiesEquatableSucceeds() {
+        let sitePluginCapabilitiesA = SitePluginCapabilities(modify: true, autoupdate: true)
         let sitePluginCapabilitiesB = SitePluginCapabilities(modify: true, autoupdate: true)
         
         XCTAssertEqual(sitePluginCapabilitiesA, sitePluginCapabilitiesB)
     }
     
     func testSitePluginCapabilitiesFails() {
+        let sitePluginCapabilitiesA = SitePluginCapabilities(modify: true, autoupdate: true)
         let sitePluginCapabilitiesB = SitePluginCapabilities(modify: false, autoupdate: false)
         
         XCTAssertNotEqual(sitePluginCapabilitiesA, sitePluginCapabilitiesB)
     }
 }
+
+
