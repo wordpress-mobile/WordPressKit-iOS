0f3583cebd31ee800b954cf24faf7df93e122739
Chip Snyder <chip.snyder3@gmail.com>
2020-11-06 11:29:19 -0500

    Include expected flag for signifying if a site design is for a blog

--- WordPressKit/RemoteSiteDesign.swift
+++ WordPressKit/RemoteSiteDesign.swift
@@ -6,6 +6,7 @@ public struct RemoteSiteDesign: Codable {
     public let demoURL: String
     public let screenshot: String?
     public let themeSlug: String?
+    public let isBlog: Bool
 
     enum CodingKeys: String, CodingKey {
         case slug
@@ -13,6 +14,7 @@ public struct RemoteSiteDesign: Codable {
         case demoURL = "demo_url"
         case screenshot
         case themeSlug = "theme"
+        case isBlog = "is_blog"
     }
 
     public init(from decoder: Decoder) throws {
@@ -22,5 +24,6 @@ public struct RemoteSiteDesign: Codable {
         demoURL = try map.decode(String.self, forKey: .demoURL)
         screenshot = try? map.decode(String.self, forKey: .screenshot)
         themeSlug = try? map.decode(String.self, forKey: .themeSlug)
+        isBlog = (try? map.decode(Bool.self, forKey: .isBlog)) ?? false
     }
 }
