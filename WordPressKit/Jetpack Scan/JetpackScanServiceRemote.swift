72321f8da1b03e7776221659f784e45c1abd9821
Emily Laguna <emily.laguna@automattic.com>
2020-11-11 16:41:42 -0500

    Add Jetpack Scan Service

---
+++ WordPressKit/Jetpack Scan/JetpackScanServiceRemote.swift
@@ -0,0 +1,71 @@
+import Foundation
+import WordPressShared
+import CocoaLumberjack
+
+
+public class JetpackScanServiceRemote: ServiceRemoteWordPressComREST {
+    /// Gets whether the site has the scan feature enabled
+    public func getScanEnabledForSite(_ siteID: Int, success: @escaping(Bool) -> Void, failure: @escaping(Error) -> Void) {
+        getScanForSite(siteID, success: { (scan) in
+            success(scan.isEnabled)
+        }, failure: failure)
+    }
+
+    /// Gets the current scan status
+    public func getCurrentScanStatusForSite(_ siteID: Int, success: @escaping(JetpackScanStatus?) -> Void, failure: @escaping(Error) -> Void) {
+        getScanForSite(siteID, success: { (scan) in
+            success(scan.current)
+        }, failure: failure)
+    }
+
+    /// Starts a scan for a site
+    public func startScanForSite(_ siteID: Int, success: @escaping(Bool) -> Void, failure: @escaping(Error) -> Void) {
+        let path = self.scanPath(for: siteID, with: "enqueue")
+
+
+        wordPressComRestApi.POST(path, parameters: nil) { (response, _) in
+            guard let responseValue = response["success"] as? Bool else {
+                success(false)
+                return
+            }
+
+            success(responseValue)
+        } failure: { (error, _) in
+            failure(error)
+        }
+    }
+
+    /// Gets the main scan object
+    public func getScanForSite(_ siteID: Int, success: @escaping(JetpackScan) -> Void, failure: @escaping(Error) -> Void) {
+        let path = self.scanPath(for: siteID)
+
+        wordPressComRestApi.GET(path, parameters: nil) { (response, _) in
+            do {
+                let decoder = JSONDecoder()
+                let data = try JSONSerialization.data(withJSONObject: response, options: [])
+                let envelope = try decoder.decode(JetpackScan.self, from: data)
+
+                success(envelope)
+            } catch {
+                failure(error)
+            }
+
+        } failure: { (error, _) in
+            failure(error)
+        }
+    }
+
+
+    // MARK: - Private
+    private func scanPath(for siteID: Int, with path: String? = nil) -> String {
+        var endpoint = "sites/\(siteID)/scan/"
+
+        if let path = path {
+            endpoint = endpoint.appending(path)
+        }
+
+        return self.path(forEndpoint: endpoint, withVersion: ._2_0)
+    }
+
+
+}
