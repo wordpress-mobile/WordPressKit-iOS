843bbb650dc861dc84cea840e56a3af2625f0357
Emily Laguna <emily.laguna@automattic.com>
2020-11-17 12:06:48 -0500

    Add JetpackThreatContext model

--- WordPressKit/Jetpack Scan/JetpackScanThreat.swift
+++ WordPressKit/Jetpack Scan/JetpackScanThreat.swift
@@ -34,11 +34,20 @@ public struct JetpackScanThreat: Decodable {
     /// More information if the threat is a extension type (plugin or theme)
     var `extension`: JetpackThreatExtension? = nil
 
+    /// If this is a file based threat this will provide code context to be displayed
+    /// Context example:
+    /// 3: start test
+    /// 4: VIRUS_SIG
+    /// 5: end test
+    /// marks: 4: (0, 9)
+    var context: JetpackThreatContext? = nil
+
     // Core modification threats will contain a git diff string
     var diff: String? = nil
 
     // Database threats will contain row information
     var rows: [String: Any]? = nil
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
@@ -52,6 +61,11 @@ public struct JetpackScanThreat: Decodable {
         `extension` = try? container.decode(JetpackThreatExtension.self, forKey: .extension)
         diff = try? container.decode(String.self, forKey: .diff)
         rows = try? container.decode([String: Any].self, forKey: .rows)
+
+        if let contextDict = try? container.decode([String: Any].self, forKey: .context) {
+            context = JetpackThreatContext(with: contextDict)
+        }
+
     }
 
     private enum CodingKeys: String, CodingKey {
@@ -62,7 +76,11 @@ public struct JetpackScanThreat: Decodable {
         case fixable
         case `extension`
         case diff
+        case context
         case rows
+    }
+}
+
 /// An object that describes how a threat can be fixed
 public struct JetpackScanThreatFixer: Decodable {
     public enum ThreatFixType: String {
@@ -135,5 +153,79 @@ public struct JetpackThreatExtension: Decodable {
         case isPremium
     }
 }
+
+public struct JetpackThreatContext {
+    public struct ThreatContextLine {
+        var lineNumber: Int
+        var contents: String
+        var highlights: [NSRange]? = nil
+    }
+
+    var lines: [ThreatContextLine]
+
+    public init?(with dict: [String: Any]){
+        guard let marksDict = dict["marks"] as? [String: Any] else {
+            return nil
+        }
+
+        var lines: [ThreatContextLine] = []
+
+        // Parse the "lines" which are represented as the keys of the dict
+        // "3", "4", "5"
+        for key in dict.keys {
+            // Since we've already pulled the marks out above, ignore it here
+            if key == "marks" {
+                continue
+            }
+
+            // Validate the incoming object to make sure it contains an integer line, and
+            // the string contents
+            guard let lineNum = Int(key), let contents = dict[key] as? String else {
+                continue
+            }
+
+            let highlights: [NSRange]? = Self.highlights(with: marksDict, for: key)
+
+            let context = ThreatContextLine(lineNumber: lineNum,
+                                            contents: contents,
+                                            highlights: highlights)
+
+            lines.append(context)
+        }
+
+        // Since the dictionary keys are unsorted, resort by line number
+        self.lines =  lines.sorted { $0.lineNumber < $1.lineNumber }
+    }
+
+    /// Parses the marks dictionary and converts them to an array of NSRange's
+    private static func highlights(with dict: [String: Any], for key: String) -> [NSRange]? {
+        guard let marks = dict[key] as? [[Double]] else {
+            return nil
+        }
+
+        var highlights: [NSRange] = []
+
+        for rangeArray in marks {
+            if let range = Self.range(with: rangeArray) {
+                highlights.append(range)
+            }
+        }
+
+        return (highlights.count > 0) ? highlights : nil
+    }
+
+    /// Generates an NSRange from an array
+    /// - Parameter array: An array that contains 2 numbers [start, length]
+    /// - Returns: Nil if the array fails validation, or an NSRange
+    private static func range(with array: [Double]) -> NSRange? {
+
+        guard array.count == 2,
+              let location = array.first,
+              let length = array.last
+        else {
+            return nil
+        }
+
+        return NSRange(location: Int(location), length: Int(length))
     }
 }
