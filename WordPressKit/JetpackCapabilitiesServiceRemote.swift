1b65a16bc064254db159d9afde13857200a1bda7
Leandro Alonso <contato@leandroalonso.com>
2021-01-13 17:49:09 -0300

    Make JetpackCapabilitiesServiceRemote open

--- WordPressKit/JetpackCapabilitiesServiceRemote.swift
+++ WordPressKit/JetpackCapabilitiesServiceRemote.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// A service that returns the Jetpack Capabilities for a set of blogs
-public class JetpackCapabilitiesServiceRemote: ServiceRemoteWordPressComREST {
+open class JetpackCapabilitiesServiceRemote: ServiceRemoteWordPressComREST {
 
 
     /// Returns a Dictionary of capabilities for each given siteID
@@ -9,7 +9,7 @@ public class JetpackCapabilitiesServiceRemote: ServiceRemoteWordPressComREST {
     ///   - siteIds: an array of Int representing siteIDs
     ///   - success: a success block that accepts a dictionary as a parameter
     ///   - failure: a failure block
-    public func `for`(siteIds: [Int], success: @escaping ([String: AnyObject]) -> Void, failure: @escaping () -> Void) {
+    open func `for`(siteIds: [Int], success: @escaping ([String: AnyObject]) -> Void, failure: @escaping () -> Void) {
 
         let endpoint = "rewind/batch-capabilities"
         let path = self.path(forEndpoint: endpoint, withVersion: ._2_0)
