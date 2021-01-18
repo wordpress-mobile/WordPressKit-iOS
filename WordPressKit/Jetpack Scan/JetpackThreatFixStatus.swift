55fcbbc19fb22c2b43d9e3e600d5dab79a0e5623
Emily Laguna <emily.laguna@automattic.com>
2021-01-18 13:00:05 -0500

    Add Jetpack scan fix status model

---
+++ WordPressKit/Jetpack Scan/JetpackThreatFixStatus.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+public struct JetpackThreatFixResponse: Decodable {
+    public let success: Bool
+    public let threats: [JetpackThreatFixStatus]
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        success = try container.decode(Bool.self, forKey: .success)
+
+        let statusDict = try container.decode([String: [String: String]].self, forKey: .threats)
+        var statusArray: [JetpackThreatFixStatus] = []
+
+        for (threatId, status) in statusDict {
+            guard let id = Int(threatId), let statusValue = status["status"] else {
+                throw ResponseError.decodingFailure
+            }
+
+            let fixStatus = JetpackThreatFixStatus(with: id, status: statusValue)
+            statusArray.append(fixStatus)
+        }
+
+        threats = statusArray
+    }
+
+
+    /// Returns true the fixing status is complete, and all threats are no longer in progress
+    public var finished: Bool {
+        return inProgress.count <= 0
+    }
+
+    /// Returns all the fixed threats
+    public var fixed: [JetpackThreatFixStatus] {
+        return threats.filter { $0.status == .fixed }
+    }
+
+    /// Returns all the in progress threats
+    public var inProgress: [JetpackThreatFixStatus] {
+        return threats.filter { $0.status == .inProgress }
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case success = "ok", threats
+    }
+
+    enum ResponseError: Error {
+        case decodingFailure
+    }
+}
+
+public struct JetpackThreatFixStatus {
+    public enum ThreatFixStatus: String, Decodable, UnknownCaseRepresentable {
+        case inProgress
+        case fixed
+
+        case unknown
+        static let unknownCase: Self = .unknown
+    }
+
+    public let threatId: Int
+    public let status: ThreatFixStatus
+
+    public init(with threatId: Int, status: String){
+        self.threatId = threatId
+        self.status = ThreatFixStatus(rawValue: status)
+    }
+}
