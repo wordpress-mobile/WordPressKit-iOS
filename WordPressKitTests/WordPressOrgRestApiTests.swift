ff8c5759143deb978180e2407d5f9a48c686ae31
Carlos Garcia <fluiddot@gmail.com>
2021-02-10 14:02:12 +0100

    Add unit test for WP.org POST request
    Apply swiftlint corrections
    
    Update `testSuccessfulPostCall` test case

--- WordPressKitTests/WordPressOrgRestApiTests.swift
+++ WordPressKitTests/WordPressOrgRestApiTests.swift
@@ -41,7 +41,7 @@ class WordPressOrgRestApiTests: XCTestCase {
         waitForExpectations(timeout: 2, handler: nil)
     }
 
-    func testSuccessfulCall() {
+    func testSuccessfulGetCall() {
         stub(condition: isAPIRequest()) { _ in
             let stubPath = OHPathForFile("wp-pages.json", type(of: self))
             return fixture(filePath: stubPath!, headers: ["Content-Type" as NSObject: "application/json" as AnyObject])
@@ -57,6 +57,36 @@ class WordPressOrgRestApiTests: XCTestCase {
                     return
                 }
                 XCTAssertEqual(pages.count, 10, "The API should return 10 pages")
+            case .failure:
+                XCTFail("This call should not fail")
+            }
+        }
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+
+    func testSuccessfulPostCall() {
+        stub(condition: isAPIRequest()) { _ in
+            let stubPath = OHPathForFile("wp-reusable-blocks.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type" as NSObject: "application/json" as AnyObject])
+        }
+        let expect = self.expectation(description: "One callback should be invoked")
+        let api = WordPressOrgRestApi(apiBase: apiBase)
+        let blockContent = "<!-- wp:paragraph -->\n<p>Some text</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:list -->\n<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>\n<!-- /wp:list -->"
+        let parameters: [String: String] = ["id": "6", "content": blockContent]
+        api.POST("wp/v2/blocks/6", parameters: parameters as [String: AnyObject]) { (result, _) in
+            expect.fulfill()
+            switch result {
+            case .success(let object):
+                guard
+                    let block = object as? [String: AnyObject],
+                    let content = block["content"] as? [String: AnyObject],
+                    let rawContent = content["raw"] as? String
+                else {
+                    XCTFail("Unexpected API result")
+                    return
+                }
+                XCTAssertEqual(block.count, 15, "The API should return the block")
+                XCTAssertEqual(rawContent, blockContent, "The API should return the block")
             case .failure:
                 XCTFail("This call should not fail")
             }
