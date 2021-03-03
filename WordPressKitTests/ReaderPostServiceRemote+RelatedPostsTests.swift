a7692a36d314170cbcbcbc8ccddf5785e6130d9b
Momo Ozawa <momo.ozawa@automattic.com>
2021-03-03 20:56:08 +0900

    Fix build warnings for tests

--- WordPressKitTests/ReaderPostServiceRemote+RelatedPostsTests.swift
+++ WordPressKitTests/ReaderPostServiceRemote+RelatedPostsTests.swift
@@ -29,10 +29,10 @@ class ReaderPostServiceRelatedPostsTests: RemoteTestCase, RESTTestable {
                            filename: "reader-post-related-posts-success.json",
                            contentType: .ApplicationJSON)
 
-        readerPostServiceRemote.fetchRelatedPosts(for: postID, from: siteID) { relatedPosts in
+        readerPostServiceRemote.fetchRelatedPosts(for: postID, from: siteID, success: { relatedPosts in
             XCTAssertTrue(relatedPosts.count == 2)
             expect.fulfill()
-        } failure: { _ in }
+        }, failure: { _ in })
 
         waitForExpectations(timeout: timeout, handler: nil)
     }
@@ -46,13 +46,13 @@ class ReaderPostServiceRelatedPostsTests: RemoteTestCase, RESTTestable {
                            contentType: .ApplicationJSON,
                            status: 503)
 
-        readerPostServiceRemote.fetchRelatedPosts(for: postID, from: siteID) { relatedPosts in
+        readerPostServiceRemote.fetchRelatedPosts(for: postID, from: siteID, success: { relatedPosts in
             XCTFail("This callback shouldn't get called")
             expect.fulfill()
-        } failure: { error in
+        }, failure: { error in
             XCTAssertNotNil(error)
             expect.fulfill()
-        }
+        })
 
         waitForExpectations(timeout: timeout, handler: nil)
     }
