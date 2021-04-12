5e5b10d2dcd64491b343ee2f8c84ffcfc1a879b8
Shiki <jayson@basanes.net>
2021-04-12 16:21:56 -0600

    Make FakeInfoDictionaryObjectProvider reusable

--- WordPressKitTests/Utilities/AppTransportSecuritySettingsTests.swift
+++ WordPressKitTests/Utilities/AppTransportSecuritySettingsTests.swift
@@ -97,19 +97,3 @@ final class AppTransportSecuritySettingsTests: XCTestCase {
         XCTAssertTrue(secureAccessOnly)
     }
 }
-
-private class FakeInfoDictionaryObjectProvider: InfoDictionaryObjectProvider {
-    private let appTransportSecurity: [String: Any]?
-
-    init(appTransportSecurity: [String: Any]?) {
-        self.appTransportSecurity = appTransportSecurity
-    }
-
-    func object(forInfoDictionaryKey key: String) -> Any? {
-        if key == "NSAppTransportSecurity" {
-            return appTransportSecurity
-        }
-
-        return nil
-    }
-}
