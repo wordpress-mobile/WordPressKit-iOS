68e61ae79774f6e9f0944d3ce5eb03f2d329fe4e
Emily Laguna <emily.laguna@automattic.com>
2021-06-11 16:08:09 -0400

    Add public initializers for PluginState, SitePlugin, and SitePluginCapabilities

--- WordPressKit/SitePlugin.swift
+++ WordPressKit/SitePlugin.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct SitePlugins: Codable {
     public var plugins: [PluginState]
     public var capabilities: SitePluginCapabilities
+
+    public init(plugins: [PluginState], capabilities: SitePluginCapabilities) {
+        self.plugins = plugins
+        self.capabilities = capabilities
+    }
 }
--- WordPressKit/SitePluginCapabilities.swift
+++ WordPressKit/SitePluginCapabilities.swift
@@ -4,6 +4,11 @@ public struct SitePluginCapabilities: Equatable, Codable {
     public let modify: Bool
     public let autoupdate: Bool
 
+    public init(modify: Bool, autoupdate: Bool) {
+        self.modify = modify
+        self.autoupdate = autoupdate
+    }
+
     public static func ==(lhs: SitePluginCapabilities, rhs: SitePluginCapabilities) -> Bool {
         return lhs.modify == rhs.modify
             && lhs.autoupdate == rhs.autoupdate
