1e1b8cbd6087ec31bbe3af0ead0fb2adc49ce05e
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-07-29 20:47:11 -0600

    Add lock file linting

--- .buildkite/lint.sh
+++ .buildkite/lint.sh
@@ -5,7 +5,25 @@ set -e
 echo "--- :rubygems: Setting up Gems"
 bundle install
 
+echo "--- :rubygems: Checking Gemfile.lock"
+# This will only work if it goes after `bundle install` – don't try to run it first
+if [ -n "$(git status | grep modified | grep Gemfile.lock)" ]; then 
+	echo "Error: Gemfile.lock is not in sync – please run \`bundle install\` and commit your changes"
+	exit 1
+fi 
+
 echo "--- :rubocop: Running Rubocop"
 bundle exec rubocop
+
+echo "Gemfile.lock is in sync"
+
+echo "--- :cocoapods: Checking Podfile.lock"
+bundle exec pod check
+
+# This will only work if it goes after `pod install` – don't try to run it first
+if [ -n "$(git status | grep modified | grep Podfile.lock)" ]; then 
+	echo "Error: Podfile.lock is not in sync – please run \`bundle exec pod install\` and commit your changes"
+	exit 1
+fi 
 
 # TODO: Add swiftlint
--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@
 source 'https://rubygems.org'
 
 gem 'cocoapods', '~> 1.10'
+gem 'cocoapods-check', '~> 1.1'
 gem 'fastlane', '~> 2.189'
 gem 'rubocop', '~> 1.18'
