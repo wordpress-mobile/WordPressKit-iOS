de5c7e138f43b9e23012fa90f5f2dd07efafc881
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-11-01 19:49:22 +0700

    Convert RemoteCommentV2 to a decodable struct

---
+++ WordPressKit/CommentServiceRemoteREST+ApiV2.swift
@@ -0,0 +1,9 @@
+//
+//  CommentServiceRemoteREST+ApiV2.swift
+//  WordPressKit
+//
+//  Created by David Christiandy on 01/11/21.
+//  Copyright © 2021 Automattic Inc. All rights reserved.
+//
+
+import Foundation
--- WordPressKit/RemoteCommentV2.swift
+++ WordPressKit/RemoteCommentV2.swift
@@ -1,19 +1,96 @@
 /// Captures the JSON structure for Comments returned from API v2 endpoint.
-@objc public class RemoteCommentV2: NSObject {
-    public var commentID: Int = 0
-    public var postID: Int = 0
+public struct RemoteCommentV2 {
+    public var commentID: Int
+    public var postID: Int
     public var parentID: Int = 0
-    public var authorID: Int = 0
-    public var authorEmail: String? // only available on edit context.
+    public var authorID: Int
     public var authorName: String?
+    public var authorEmail: String? // only available in edit context
     public var authorURL: String?
-    public var authorIP: String? // only available on edit context.
-    public var authorUserAgent: String? // only available on edit context.
+    public var authorIP: String? // only available in edit context
+    public var authorUserAgent: String? // only available in edit context
     public var authorAvatarURL: String?
-    public var date: NSDate?
-    public var content: String?
-    public var rawContent: String?
-    public var link: String?
-    public var status: String?
-    public var type: String?
+    public var date: Date
+    public var content: String
+    public var rawContent: String? // only available in edit context
+    public var link: String
+    public var status: String
+    public var type: String
+}
+
+// MARK: - Decodable
+
+extension RemoteCommentV2: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case post
+        case parent
+        case author
+        case authorName = "author_name"
+        case authorEmail = "author_email"
+        case authorURL = "author_url"
+        case authorIP = "author_ip"
+        case authorUserAgent = "author_user_agent"
+        case date = "date_gmt"
+        case content
+        case authorAvatarURLs = "author_avatar_urls"
+        case link
+        case status
+        case type
+    }
+
+    enum ContentKeys: String, CodingKey {
+        case rendered
+        case raw
+    }
+
+    enum AuthorAvatarKeys: String, CodingKey {
+        case size96 = "96"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.commentID = try container.decode(Int.self, forKey: .id)
+        self.postID = try container.decode(Int.self, forKey: .post)
+        self.parentID = try container.decode(Int.self, forKey: .parent)
+        self.authorID = try container.decode(Int.self, forKey: .author)
+        self.authorName = try container.decode(String.self, forKey: .authorName)
+        self.authorEmail = try container.decodeIfPresent(String.self, forKey: .authorEmail)
+        self.authorURL = try container.decode(String.self, forKey: .authorURL)
+        self.authorIP = try container.decodeIfPresent(String.self, forKey: .authorIP)
+        self.authorUserAgent = try container.decodeIfPresent(String.self, forKey: .authorUserAgent)
+
+        guard let dateString = try? container.decode(String.self, forKey: .date),
+           let date = NSDate(wordPressComJSONString: dateString) as Date? else {
+            throw DecodingError.dataCorruptedError(forKey: .date, in: container, debugDescription: "Date parsing failed")
+        }
+
+        self.date = date
+
+        let contentContainer = try container.nestedContainer(keyedBy: ContentKeys.self, forKey: .content)
+        self.rawContent = try contentContainer.decodeIfPresent(String.self, forKey: .raw)
+        self.content = try contentContainer.decode(String.self, forKey: .rendered)
+
+        self.link = try container.decode(String.self, forKey: .link)
+
+        let remoteStatus = try container.decode(String.self, forKey: .status)
+        self.status = Self.status(from: remoteStatus)
+        self.type = try container.decode(String.self, forKey: .type)
+
+        let avatarContainer = try container.nestedContainer(keyedBy: AuthorAvatarKeys.self, forKey: .authorAvatarURLs)
+        self.authorAvatarURL = try avatarContainer.decode(String.self, forKey: .size96)
+    }
+
+    private static func status(from remoteStatus: String) -> String {
+        switch remoteStatus {
+        case "unapproved":
+            return "hold"
+        case "approved":
+            return "approve"
+        default:
+            return remoteStatus
+        }
+    }
 }
