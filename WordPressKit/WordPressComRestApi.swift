9100d5423d72e159a54d32a77960fa28212907cd
Leandro Alonso <contato@leandroalonso.com>
2022-01-03 12:07:01 -0300

    refactor: make sure key and value are both encoded

--- WordPressKit/WordPressComRestApi.swift
+++ WordPressKit/WordPressComRestApi.swift
@@ -692,9 +692,13 @@ extension ProgressUserInfoKey {
 
 private extension Dictionary {
     func percentEncoded() -> Data? {
-        return map { key, value in
-            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+        return compactMap { key, value in
+            guard
+                let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed),
+                let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed)
+            else {
+                return .none
+            }
             return escapedKey + "=" + escapedValue
         }
         .joined(separator: "&")
