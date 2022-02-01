0f5bda1184d1c36de3d49122ab86f8d25f139005
Leandro Alonso <contato@leandroalonso.com>
2022-02-01 15:20:21 -0300

    add: initial request to the endpoint

--- WordPressKit/DashboardServiceRemote.swift
+++ WordPressKit/DashboardServiceRemote.swift
@@ -1,5 +1,31 @@
 import Foundation
 
 open class DashboardServiceRemote: ServiceRemoteWordPressComREST {
-    
+    open func fetch(cards: [String], forBlogID blogID: Int, completion: @escaping () -> Void) {
+        guard let requestUrl = endpoint(for: cards, blogID: blogID) else {
+            return
+        }
+
+        wordPressComRestApi.GET(requestUrl,
+                                parameters: nil,
+                                success: { response, _ in
+
+                                    completion()
+        }, failure: { error, _ in
+            DDLogError("Error fetching dashboard cards: \(error)")
+        })
+    }
+
+    private func endpoint(for cards: [String], blogID: Int) -> String? {
+        var path = URLComponents(string: "sites/\(blogID)/dashboard/cards/v1_1/")
+
+        let cardsEncoded = cards.joined(separator: ",")
+        path?.queryItems = [URLQueryItem(name: "cards", value: cardsEncoded)]
+
+        guard let endpoint = path?.string else {
+            return nil
+        }
+
+        return self.path(forEndpoint: endpoint, withVersion: ._2_0)
+    }
 }
--- WordPressKitTests/DashboardServiceRemoteTests.swift
+++ WordPressKitTests/DashboardServiceRemoteTests.swift
@@ -5,4 +5,21 @@ import XCTest
 class DashboardServiceRemoteTests: RemoteTestCase, RESTTestable {
     let mockRemoteApi = MockWordPressComRestApi()
     var dashboardServiceRemote: DashboardServiceRemote!
+
+    override func setUp() {
+        dashboardServiceRemote = DashboardServiceRemote(wordPressComRestApi: getRestApi())
+    }
+
+    // Request the correct set of cards
+    //
+    func testRequestCards() {
+        let expect = expectation(description: "Get cards successfully")
+        stubRemoteResponse("wpcom/v2/sites/165243437/dashboard/cards/v1_1/?cards=posts,today_stats", filename: "dashboard-200-with-drafts-and-scheduled-posts.json", contentType: .ApplicationJSON)
+
+        dashboardServiceRemote.fetch(cards: ["posts", "today_stats"], forBlogID: 165243437) {
+            expect.fulfill()
+        }
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
 }
