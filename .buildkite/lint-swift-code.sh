2526042b8e0a84d918e3f3ab029d5784ed63190d
Gio Lodi <giovanni.lodi42@gmail.com>
2022-06-07 15:02:33 +1000

    Annotate build with Danger output if failed

--- .buildkite/lint-swift-code.sh
+++ .buildkite/lint-swift-code.sh
@@ -18,4 +18,23 @@ echo "--- :cocoapods: Setting up Pods"
 install_cocoapods
 
 echo "--- :swift::danger: Running SwiftLint via Danger"
-bundle exec danger
+set +e
+bundle exec danger > danger_output
+DANGER_EXIT_CODE=$?
+
+echo "Danger finished with: $DANGER_EXIT_CODE"
+if [ "$DANGER_EXIT_CODE" -ne 0 ] ; then
+    echo "Danger failed the build"
+    # FIXME: ShellCheck tells us this cat is useless (SC2002)
+    cat danger_output | buildkite-agent annotate \
+      --style 'error' \
+      --context 'ctx-error' \
+    exit 1
+fi
+
+# Now that we don't need to trap error codes to annotate the build with extra
+# info, fail the script if a commands it calls fails.
+#
+# It's sort of unnecessary to do this here because the script is finished, but
+# useful in case we'll add code in the future.
+set -e
