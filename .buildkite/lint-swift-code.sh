a91eeaf4d55d5ea2a2949c7fe71345885d136e7a
Gio Lodi <giovanni.lodi42@gmail.com>
2022-06-07 10:29:46 +1000

    DEBUG – Danger token not there, what about GitHub?

--- .buildkite/lint-swift-code.sh
+++ .buildkite/lint-swift-code.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -eu
 
-# See https://github.com/Automattic/bash-cache-buildkite-plugin/issues/16
-gem install bundler:2.3.4
+if [[ -z $GITHUB_TOKEN ]]; then
+  echo "Can't find GITHUB_TOKEN"
+fi
 
 if [[ -z $DANGER_GITHUB_API_TOKEN ]]; then
   echo "Can't find DANGER_GITHUB_API_TOKEN"
 fi
 
-if [[ -z $GITHUB_TOKEN ]]; then
-  echo "Can't find GITHUB_TOKEN"
-fi
+# See https://github.com/Automattic/bash-cache-buildkite-plugin/issues/16
+gem install bundler:2.3.4
 
 bundle install
 bundle exec danger
