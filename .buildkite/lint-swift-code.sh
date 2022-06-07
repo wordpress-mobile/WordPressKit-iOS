0c3ce7ca72e3689157b7b33778370035df57026c
Gio Lodi <giovanni.lodi42@gmail.com>
2022-06-07 11:25:08 +1000

    TEST – Can we access other secrets?

--- .buildkite/lint-swift-code.sh
+++ .buildkite/lint-swift-code.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+if [[ -z $COCOAPODS_TRUNK_TOKEN ]]; then
+  echo "Can't find COCOAPODS_TRUNK_TOKEN" # Of course, this will fail because of the `-u` in the shebang
+fi
+
 if [[ -z $DANGER_GITHUB_API_TOKEN ]]; then
   echo "Can't find DANGER_GITHUB_API_TOKEN"
 fi
