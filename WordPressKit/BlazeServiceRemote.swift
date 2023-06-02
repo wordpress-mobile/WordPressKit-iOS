1f791e7e8970be129ff4c4e0075419d888b08bc8
kean <grebenyuk.alexander@gmail.com>
2023-06-02 15:04:56 -0400

    Add blaze search campaigns endpoint

--- WordPressKit/BlazeServiceRemote.swift
+++ WordPressKit/BlazeServiceRemote.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-open class BlazeServiceRemote: ServiceRemoteWordPressComREST {
+public final class BlazeServiceRemote: ServiceRemoteWordPressComREST {
 
     public typealias BlazeStatusResponseCallback = (Result<Bool, Error>) -> Void
 
@@ -10,7 +10,7 @@ open class BlazeServiceRemote: ServiceRemoteWordPressComREST {
 
     // MARK: - Status
 
-    open func getStatus(forSiteId siteId: Int, callback: @escaping BlazeStatusResponseCallback) {
+    public func getStatus(forSiteId siteId: Int, callback: @escaping BlazeStatusResponseCallback) {
 
         let endpoint = "sites/\(siteId)/blaze/status"
         let path = self.path(forEndpoint: endpoint, withVersion: ._2_0)
@@ -34,6 +34,29 @@ open class BlazeServiceRemote: ServiceRemoteWordPressComREST {
 
             callback(.failure(error))
         })
-
+    }
+    
+    // MARK: - Campaigns
+    
+    func getCampaigns(forSiteId siteId: Int, page: Int = 1, callback: @escaping (Result<BlazeCampaignsResponse, Error>) -> Void) {
+        let endpoint = "sites/\(siteId)/wordads/dsp/api/v1/search/campaigns"
+        let path = self.path(forEndpoint: endpoint, withVersion: ._2_0)
+        wordPressComRestApi.GET(path, parameters: [
+            "order": "asc" as AnyObject,
+            "order_by": "post_date" as AnyObject,
+            "page": "\(page)" as AnyObject,
+            "site_id": "\(siteId)" as AnyObject
+        ], success: { response, _ in
+            do {
+                let data = try JSONSerialization.data(withJSONObject: response)
+                let response = try JSONDecoder.apiDecoder.decode(BlazeCampaignsResponse.self, from: data)
+                callback(.success((response)))
+            } catch {
+                WPKitLogError("Error parsing campaigns response: \(error), \(response)")
+                callback(.failure(error))
+            }
+        }, failure: { error, _ in
+            callback(.failure(error))
+        })
     }
 }
--- WordPressKitTests/BlazeServiceRemoteTests.swift
+++ WordPressKitTests/BlazeServiceRemoteTests.swift
@@ -2,79 +2,109 @@ import XCTest
 @testable import WordPressKit
 
 final class BlazeServiceRemoteTests: RemoteTestCase, RESTTestable {
-
+    
     // MARK: - Constants
-
+    
     let siteId = 1
-
+    
     // MARK: - Properties
-
+    
     var statusEndpoint: String { return "sites/\(siteId)/blaze/status" }
     var service: BlazeServiceRemote!
-
+    
     // MARK: - Tests
-
+    
     func testGetStatusReturnsSuccess() throws {
         // Given
         let status = ["approved": true]
         let data = try JSONEncoder().encode(status)
         stubRemoteResponse(statusEndpoint, data: data, contentType: .ApplicationJSON)
-
+        
         // When
         let expectation = XCTestExpectation()
         BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
+            
             // Then
             let approved = try! result.get()
             XCTAssertEqual(approved, true)
             expectation.fulfill()
-
+            
         }
-
+        
         wait(for: [expectation], timeout: 1)
     }
-
+    
     func testMalformedResponseReturnsError() throws {
         // Given
         let data = try toJSON(object: ["Invalid"])
         stubRemoteResponse(statusEndpoint, data: data, contentType: .ApplicationJSON)
-
+        
         // When
         let expectation = XCTestExpectation()
         BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
+            
             // Then
             switch result {
-                case .success: XCTFail()
-                case .failure: expectation.fulfill()
+            case .success: XCTFail()
+            case .failure: expectation.fulfill()
             }
-
+            
         }
-
+        
         wait(for: [expectation], timeout: 1)
     }
-
+    
     func testGetStatusReturnsFailure() {
         // Given
         stubRemoteResponse(statusEndpoint, data: Data(), contentType: .NoContentType, status: 403)
-
+        
         // When
         let expectation = XCTestExpectation()
         BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
+            
             // Then
             if case .success = result {
                 XCTFail()
             }
             expectation.fulfill()
         }
-
+        
         wait(for: [expectation], timeout: 1)
     }
-
+    
     private func toJSON<T: Codable>(object: T) throws -> Data {
         let encoder = JSONEncoder()
         encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
         return try encoder.encode(object)
+    }
+    
+    // MARK: - Campaigns
+    
+    func testDecodeCampaigns() throws {
+        // Given
+        let url = try XCTUnwrap(Bundle(for: BlazeServiceRemoteTests.self).url(forResource: "blaze-campaigns", withExtension: "json"))
+        let data = try Data(contentsOf: url)
+    
+        // When
+        let response = try JSONDecoder.apiDecoder.decode(BlazeCampaignsResponse.self, from: data)
+        
+        // Then
+        XCTAssertEqual(response.totalItems, 1)
+        XCTAssertEqual(response.totalPages, 1)
+        XCTAssertEqual(response.page, 1)
+        XCTAssertEqual(response.campaigns?.count, 1)
+        
+        let campaign = try XCTUnwrap(response.campaigns?.first)
+        XCTAssertEqual(campaign.campaignID, 26916)
+        XCTAssertEqual(campaign.name, "Test Post - don't approve")
+        XCTAssertEqual(campaign.startDate, ISO8601DateFormatter().date(from: "2023-06-13T00:00:00Z"))
+        XCTAssertEqual(campaign.endDate, ISO8601DateFormatter().date(from: "2023-06-01T19:15:45Z"))
+        XCTAssertEqual(campaign.status, .canceled)
+        XCTAssertEqual(campaign.budgetCents, 500)
+        XCTAssertEqual(campaign.targetURL, "https://alextest9123.wordpress.com/2023/06/01/test-post/")
+        XCTAssertEqual(campaign.contentConfig?.title, "Test Post - don't approve")
+        XCTAssertEqual(campaign.contentConfig?.snippet, "Test Post Empty Empty")
+        XCTAssertEqual(campaign.contentConfig?.clickURL, "https://alextest9123.wordpress.com/2023/06/01/test-post/")
+        XCTAssertEqual(campaign.contentConfig?.imageURL, "https://i0.wp.com/public-api.wordpress.com/wpcom/v2/wordads/dsp/api/v1/dsp/creatives/56259/image?w=600&zoom=2")
     }
 }
