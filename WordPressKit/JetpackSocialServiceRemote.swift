70f3d384217af796db2aee5e1790c00c52d7bb54
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2023-06-06 23:12:34 +0700

    Add back the optional success type for cleanup purposes later

--- WordPressKit/JetpackSocialServiceRemote.swift
+++ WordPressKit/JetpackSocialServiceRemote.swift
@@ -9,14 +9,14 @@ public class JetpackSocialServiceRemote: ServiceRemoteWordPressComREST {
     ///   - siteID: The target site's dotcom ID.
     ///   - completion: Closure to be called once the request completes.
     public func fetchPublicizeInfo(for siteID: Int,
-                                   completion: @escaping (Result<RemotePublicizeInfo, Error>) -> Void) {
+                                   completion: @escaping (Result<RemotePublicizeInfo?, Error>) -> Void) {
         let path = path(forEndpoint: "sites/\(siteID)/jetpack-social", withVersion: ._2_0)
         wordPressComRestApi.GET(path, parameters: nil) { result, response in
             switch result {
             case .success(let responseObject):
                 do {
                     let data = try JSONSerialization.data(withJSONObject: responseObject)
-                    let info = try JSONDecoder.apiDecoder.decode(RemotePublicizeInfo.self, from: data)
+                    let info = try? JSONDecoder.apiDecoder.decode(RemotePublicizeInfo.self, from: data)
                     completion(.success(info))
                 } catch {
                     completion(.failure(error))
