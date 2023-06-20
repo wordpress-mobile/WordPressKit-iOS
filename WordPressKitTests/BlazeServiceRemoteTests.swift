540eb837a9b5e0ec464b9b523debb1b924784b11
Momo Ozawa <momo.ozawa@automattic.com>
2023-06-20 16:39:20 +0100

    remove blaze/status endpoint

--- WordPressKitTests/BlazeServiceRemoteTests.swift
+++ WordPressKitTests/BlazeServiceRemoteTests.swift
@@ -9,74 +9,8 @@ final class BlazeServiceRemoteTests: RemoteTestCase, RESTTestable {
 
     // MARK: - Properties
 
-    var statusEndpoint: String { "sites/\(siteId)/blaze/status" }
     var searchEndpoint: String { "sites/\(siteId)/wordads/dsp/api/v1/search/campaigns/site/\(siteId)" }
     var service: BlazeServiceRemote!
-
-    // MARK: - Get Status
-
-    func testGetStatusReturnsSuccess() throws {
-        // Given
-        let status = ["approved": true]
-        let data = try JSONEncoder().encode(status)
-        stubRemoteResponse(statusEndpoint, data: data, contentType: .ApplicationJSON)
-
-        // When
-        let expectation = XCTestExpectation()
-        BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
-            // Then
-            let approved = try! result.get()
-            XCTAssertEqual(approved, true)
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
-    }
-
-    func testMalformedResponseReturnsError() throws {
-        // Given
-        let data = try toJSON(object: ["Invalid"])
-        stubRemoteResponse(statusEndpoint, data: data, contentType: .ApplicationJSON)
-
-        // When
-        let expectation = XCTestExpectation()
-        BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
-            // Then
-            switch result {
-            case .success: XCTFail()
-            case .failure: expectation.fulfill()
-            }
-
-        }
-
-        wait(for: [expectation], timeout: 1)
-    }
-
-    func testGetStatusReturnsFailure() {
-        // Given
-        stubRemoteResponse(statusEndpoint, data: Data(), contentType: .NoContentType, status: 403)
-
-        // When
-        let expectation = XCTestExpectation()
-        BlazeServiceRemote(wordPressComRestApi: getRestApi()).getStatus(forSiteId: siteId) { result in
-
-            // Then
-            if case .success = result {
-                XCTFail()
-            }
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
-    }
-
-    private func toJSON<T: Codable>(object: T) throws -> Data {
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
-        return try encoder.encode(object)
-    }
 
     // MARK: - Campaigns
 
