8b8d8f9b133aae9d5fe9441c9ab3726f66d2642e
alpavanoglu <alp.avanoglu@automattic.com>
2023-06-23 18:35:52 +0200

    Add `IPLocationRemoteTests`

--- WordPressKit/IPLocationRemote.swift
+++ WordPressKit/IPLocationRemote.swift
@@ -2,31 +2,35 @@ import Foundation
 
 /// Remote type to fetch the user's IP Location using the public `geo` API.
 ///
-public final class IPLocationRemote: ServiceRemoteWordPressComREST {
+public final class IPLocationRemote {
     private enum Constants {
         static let jsonDecoder = JSONDecoder()
+    }
+
+    private let urlSession: URLSession
+
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
     }
 
     /// Fetches the country code from the device ip.
     ///
     public func fetchIPCountryCode(completion: @escaping (Result<String, Error>) -> Void) {
-
         let path = WordPressComOAuthClient.WordPressComOAuthDefaultApiBaseUrl + "/geo/"
         guard let url = URL(string: path) else {
             return completion(.failure(IPLocationError.malformedURL))
         }
 
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        let task = urlSession.dataTask(with: request) { data, _, error in
             guard let data else {
                 return
             }
-            
+
             do {
                 let result = try Constants.jsonDecoder.decode(RemoteIPCountryCode.self, from: data)
                 completion(.success(result.countryCode))
             } catch {
-                print(error.localizedDescription)
                 completion(.failure(error))
             }
         }
