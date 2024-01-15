bedee4d9d794217971cd898685c82a48228db2b8
Tony Li <tony.li@automattic.com>
2024-01-15 15:43:09 +1300

    Rewrite a test case due to WordPressComRestApiError not longer conform to Error

--- WordPressKitTests/WordPressComRestApiTests+Error.swift
+++ WordPressKitTests/WordPressComRestApiTests+Error.swift
@@ -7,16 +7,11 @@ class WordPressComRestApiErrorTests: XCTestCase {
 
     func testNSErrorBridging() {
         for error in WordPressComRestApiErrorCode.allCases {
-            let oldNSError = error as NSError
-
             let apiError = WordPressAPIError.endpointError(WordPressComRestApiEndpointError(code: error))
             let newNSError = apiError as NSError
 
-            XCTAssertEqual(oldNSError.domain, "WordPressKit.WordPressComRestApiError")
-            XCTAssertEqual(oldNSError.domain, newNSError.domain)
-
-            XCTAssertEqual(oldNSError.code, error.rawValue)
-            XCTAssertEqual(oldNSError.code, newNSError.code)
+            XCTAssertEqual(newNSError.domain, "WordPressKit.WordPressComRestApiError")
+            XCTAssertEqual(newNSError.code, error.rawValue)
         }
     }
 
