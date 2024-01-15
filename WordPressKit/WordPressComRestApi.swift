3f13aa6aebf994aa2d2fb366fe22febefc55dfa1
Tony Li <tony.li@automattic.com>
2024-01-16 12:29:49 +1300

    Decouple WP.com REST API error handling from Alamofire

--- WordPressKit/WordPressComRestApi.swift
+++ WordPressKit/WordPressComRestApi.swift
@@ -484,10 +484,21 @@ extension WordPressComRestApi {
 
     /// A custom error processor to handle error responses when status codes are betwen 400 and 500
     func processError(response: DataResponse<Any>, originalError: Error) -> WordPressComRestApiEndpointError? {
-        guard let afError = originalError as?  AFError, case AFError.responseValidationFailed(_) = afError, let httpResponse = response.response, (400...500).contains(httpResponse.statusCode), let data = response.data else {
-            if let afError = originalError as? AFError, case AFError.responseSerializationFailed(_) = afError {
-                return .init(code: .responseSerializationFailed)
-            }
+        if let afError = originalError as? AFError, case AFError.responseSerializationFailed(_) = afError {
+            return .init(code: .responseSerializationFailed)
+        }
+
+        guard let httpResponse = response.response, let data = response.data else {
+            return nil
+        }
+
+        return processError(response: httpResponse, body: data, additionalUserInfo: (originalError as NSError).userInfo)
+    }
+
+    func processError(response httpResponse: HTTPURLResponse, body data: Data, additionalUserInfo: [String: Any]?) -> WordPressComRestApiEndpointError? {
+        // Not sure if it's intentional to include 500 status code, but the code seems to be there from the very beginning.
+        // https://github.com/wordpress-mobile/WordPressKit-iOS/blob/1.0.1/WordPressKit/WordPressComRestApi.swift#L374
+        guard (400...500).contains(httpResponse.statusCode) else {
             return nil
         }
 
@@ -534,7 +545,7 @@ extension WordPressComRestApi {
             }
         }
 
-        var originalErrorUserInfo = (originalError as NSError).userInfo
+        var originalErrorUserInfo = additionalUserInfo ?? [:]
         originalErrorUserInfo.removeValue(forKey: NSLocalizedDescriptionKey)
 
         return .init(
