8a286de9057771a407f9814fb35d5346d8690254
kean <grebenyuk.alexander@gmail.com>
2024-01-24 15:25:37 -0500

    Fix getSiteErrorLogs parsing issues

--- WordPressKit/AtomicSiteServiceRemote.swift
+++ WordPressKit/AtomicSiteServiceRemote.swift
@@ -1,30 +1,32 @@
 import Foundation
 
-public final class AtomicSiteService: ServiceRemoteWordPressComREST {
+public final class AtomicSiteServiceRemote: ServiceRemoteWordPressComREST {
     /// - parameter scrollID: Pass the scroll ID from the previous response to
     /// fetch the next page.
-    public func getSiteErrorLogs(
-        siteID: Int,
-        range: Range<Date>,
-        pageSize: Int = 50,
-        severity: AtomicLogMessage.Severity? = nil,
-        scrollID: String? = nil,
-        success: @escaping (AtomicErrorLogsResponse) -> Void,
-        failure: @escaping (Error) -> Void) 
-    {
+    public func getSiteErrorLogs(siteID: Int,
+                                 range: Range<Date>,
+                                 severity: AtomicLogMessage.Severity? = nil,
+                                 scrollID: String? = nil,
+                                 pageSize: Int = 50,
+                                 success: @escaping (AtomicErrorLogsResponse) -> Void,
+                                 failure: @escaping (Error) -> Void) {
         let path = self.path(forEndpoint: "sites/\(siteID)/hosting/error-logs/", withVersion: ._2_0)
         var parameters = [
-//            "start": "\(range.lowerBound.timeIntervalSince1970)",
-//            "end": "\(range.upperBound.timeIntervalSince1970)",
-            "sort_order": "desc"
-//            "page_size": "\(pageSize)"
+            "start": "\(Int(range.lowerBound.timeIntervalSince1970))",
+            "end": "\(Int(range.upperBound.timeIntervalSince1970))",
+            "sort_order": "desc",
+            "page_size": "\(pageSize)"
         ] as [String: AnyObject]
         if let severity {
             parameters["filter[severity][]"] = severity.rawValue as AnyObject
         }
+        if let scrollID {
+            parameters["scroll_id"] = scrollID as AnyObject
+        }
         wordPressComRestApi.GET(path, parameters: parameters) { responseObject, httpResponse in
-            guard (200..<300).contains(responseObject.statusCode ?? 0),
-                  let data = responseObject as? [String: AnyObject] else {
+            guard (200..<300).contains(httpResponse?.statusCode ?? 0),
+                  let data = (responseObject as? [String: AnyObject])?["data"],
+                  JSONSerialization.isValidJSONObject(data) else {
                 failure(URLError(.unknown))
                 return
             }
