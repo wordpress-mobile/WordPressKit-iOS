b71d78422bbf26915e3c97c408dcb7602b222caa
Gio Lodi <gio@mokacoding.com>
2024-02-27 16:36:54 +1100

    WIP n.4 – Track missing CoreAPI header (is it even needed?)

---
+++ CoreAPI/CoreAPI.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreAPI.
+FOUNDATION_EXPORT double CoreAPIVersionNumber;
+
+//! Project version string for CoreAPI.
+FOUNDATION_EXPORT const unsigned char CoreAPIVersionString[];
