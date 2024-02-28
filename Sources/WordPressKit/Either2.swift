a6de8c37a50da7ee21035fd1a02807307f029f0a
Gio Lodi <gio@mokacoding.com>
2024-02-28 20:03:31 +1100

    Rename one `Either.swift` to `Either2.swift` to please CocoaPods

--- Sources/WordPressKit/Either2.swift
+++ Sources/WordPressKit/Either2.swift
@@ -1,6 +1,8 @@
 // Note that this is duplicated between some targets.
 //
 // We could use a third-party shared implementation, but given it's so simple to implement copy-paste will do for the moment.
+//
+// Update: CocoaPods complains about the file name being reused, which is why this is called Either2.swift
 enum Either<L, R> {
     case left(L)
     case right(R)
