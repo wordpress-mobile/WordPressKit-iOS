ae02cb8f6ecf8c2ff9c01ad9c814e2e686a8a099
Chris McGraw <2454408+wargcm@users.noreply.github.com>
2024-03-07 14:02:47 -0500

    Add streams API endpoint for fetching Reader cards

--- WordPressKitTests/ReaderPostServiceRemote+CardsTests.swift
+++ WordPressKitTests/ReaderPostServiceRemote+CardsTests.swift
@@ -11,6 +11,8 @@ class ReaderPostServiceRemoteCardTests: RemoteTestCase, RESTTestable {
         readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: getRestApi())
     }
 
+    // MARK: - Tags fetch cards
+
     // Return an array of cards
     //
     func testReturnCards() {
@@ -139,6 +141,142 @@ class ReaderPostServiceRemoteCardTests: RemoteTestCase, RESTTestable {
         stubRemoteResponse("read/tags/cards?tags%5B%5D=cats&sort=date", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
 
         readerPostServiceRemote.fetchCards(for: ["cats"], sortingOption: .date, success: { cards, _ in
+            let posts = cards.filter { $0.type == .post }
+            for i in 1..<posts.count {
+                guard let firstPostDate = posts[i-1].post?.sortDate,
+                      let secondPostDate = posts[i].post?.sortDate,
+                      firstPostDate > secondPostDate else {
+                    XCTFail("Posts should be sorted by date, starting with most recent post")
+                    return
+                }
+            }
+            expect.fulfill()
+        }, failure: { _ in })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    // MARK: - Streams fetch cards
+
+    func testStreamsReturnCards() {
+        let expect = expectation(description: "Get cards successfully")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=dogs", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["dogs"], success: { cards, _ in
+            XCTAssertTrue(cards.count == 10)
+            expect.fulfill()
+        }, failure: { _ in })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testStreamsReturnPosts() {
+        let expect = expectation(description: "Get cards successfully")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=cats", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["cats"], success: { cards, _ in
+            let postCards = cards.filter { $0.type == .post }
+            XCTAssertTrue(postCards.allSatisfy { $0.post != nil })
+            expect.fulfill()
+        }, failure: { _ in })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testStreamsReturnCorrectCardType() {
+        let expect = expectation(description: "Get cards successfully")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=cats", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["cats"], success: { cards, _ in
+            let postTypes = cards.map { $0.type }
+            let expectedPostTypes: [RemoteReaderCard.CardType] = [.interests, .sites, .post, .post, .post, .post, .post, .post, .post, .post]
+            XCTAssertTrue(postTypes == expectedPostTypes)
+            expect.fulfill()
+        }, failure: { _ in })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testStreamsReturnError() {
+        let expect = expectation(description: "Get cards successfully")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=cats", filename: "reader-cards-success.json", contentType: .ApplicationJSON, status: 503)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["cats"], success: { _, _ in }, failure: { error in
+            XCTAssertNotNil(error)
+            expect.fulfill()
+        })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testStreamsReturnNextPageHandle() {
+        let expect = expectation(description: "Returns next page handle")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=dogs", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["dogs"], success: { _, nextPageHandle in
+            XCTAssertTrue(nextPageHandle == "ZnJvbT0xMCZiZWZvcmU9MjAyMC0wNy0yNlQxMyUzQTU1JTNBMDMlMkIwMSUzQTAw")
+            expect.fulfill()
+        }, failure: { _ in })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testStreamsCallAPIWithTheGivenPageHandle() {
+        let readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: mockRemoteApi)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["dogs"], page: "foobar", success: { _, _ in }, failure: { _ in })
+
+        XCTAssertTrue(mockRemoteApi.getMethodCalled)
+
+        XCTAssertTrue(mockRemoteApi.URLStringPassedIn?.contains("&page_handle=foobar") ?? false)
+    }
+
+    func testStreamsCallAPIWithPopularityAsTheGivenSortingOption() {
+        let readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: mockRemoteApi)
+
+        readerPostServiceRemote.fetchStreamCards(for: [], sortingOption: .popularity, success: { _, _ in }, failure: { _ in })
+
+        XCTAssertTrue(mockRemoteApi.getMethodCalled)
+
+        XCTAssertTrue(mockRemoteApi.URLStringPassedIn?.contains("sort=popularity") ?? false)
+    }
+
+    func testStreamsCallAPIWithDateAsTheGivenSortingOption() {
+        let readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: mockRemoteApi)
+
+        readerPostServiceRemote.fetchStreamCards(for: [], sortingOption: .date, success: { _, _ in }, failure: { _ in })
+
+        XCTAssertTrue(mockRemoteApi.getMethodCalled)
+
+        XCTAssertTrue(mockRemoteApi.URLStringPassedIn?.contains("sort=date") ?? false)
+    }
+
+    func testStreamsCallAPIWithoutTheGivenSortingOption() {
+        let readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: mockRemoteApi)
+
+        readerPostServiceRemote.fetchStreamCards(for: [], success: { _, _ in }, failure: { _ in })
+
+        XCTAssertTrue(mockRemoteApi.getMethodCalled)
+
+        XCTAssertFalse(mockRemoteApi.URLStringPassedIn?.contains("sort=") ?? true)
+    }
+
+    func testStreamsCallAPIWithCountValue() {
+        let readerPostServiceRemote = ReaderPostServiceRemote(wordPressComRestApi: mockRemoteApi)
+        let expectedCount = 5
+
+        readerPostServiceRemote.fetchStreamCards(for: ["dogs"], count: expectedCount, success: { _, _ in }, failure: { _ in })
+
+        XCTAssertTrue(mockRemoteApi.getMethodCalled)
+
+        XCTAssertTrue(mockRemoteApi.URLStringPassedIn?.contains("&count=\(expectedCount)") ?? false)
+    }
+
+    func testStreamsPostsInCallAPIWithDateAsGivenSortOption() {
+        let expect = expectation(description: "Get cards sorted by date")
+        stubRemoteResponse("read/streams/discover?tags%5B%5D=cats&sort=date", filename: "reader-cards-success.json", contentType: .ApplicationJSON)
+
+        readerPostServiceRemote.fetchStreamCards(for: ["cats"], sortingOption: .date, success: { cards, _ in
             let posts = cards.filter { $0.type == .post }
             for i in 1..<posts.count {
                 guard let firstPostDate = posts[i-1].post?.sortDate,
