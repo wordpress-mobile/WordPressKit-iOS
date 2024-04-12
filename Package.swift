03d2de8a6080b70465dd16b9b77c29ddab335d35
Gio Lodi <gio@mokacoding.com>
2024-04-13 08:13:38 +1000

    Point to WordPressShared without version fallback

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // .package(url: "https://github.com/wordpress-mobile/WordPress-iOS-Shared.git", from: "2.3.1"),
         // See https://github.com/wordpress-mobile/WordPress-iOS-Shared/pull/354
-        .package(url: "https://github.com/wordpress-mobile/WordPress-iOS-Shared.git", branch: "mokagio/swiftlint-read-as-dependency"),
+        .package(url: "https://github.com/wordpress-mobile/WordPress-iOS-Shared.git", branch: "mokagio/swift-5.10-toolchain"),
         .package(url: "https://github.com/wordpress-mobile/wpxmlrpc", from: "0.10.0"),
         // Test dependencies
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0"),
