4fbd973b499f1312103311ff5c883fba19c44dc6
Paolo Musolino <info@codeido.com>
2025-03-19 12:45:05 +0100

    update: Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WordPressKit",
-            url: "https://github.com/user-attachments/files/19338447/WordPressKit.zip",
-            checksum: "309f0960e8881e174bec2f9c0f2d833c6c5926e952394bfa87eb532d92709eac"
+            url: "https://github.com/user-attachments/files/19339848/WordPressKit.zip",
+            checksum: "5bf1ff361992dccf44dfe41b0a442ee4c3c13dc0a1ce9b647a8ed1b976b5c3fc"
         ),
     ]
 )
