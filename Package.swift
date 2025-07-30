b62cdff9a84986b7bc7b54c176b07a341204ce56
Tony Li <tony.li@automattic.com>
2025-07-31 10:31:33 +1200

    Update xcframework

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WordPressKit",
-            url: "https://github.com/user-attachments/files/20895757/WordPressKit.zip",
-            checksum: "b08eaf182f0399303aadccb1a6dad6cad294a9c8d123d920889b15950c85e08f"
+            url: "https://github.com/user-attachments/files/21518814/WordPressKit.zip",
+            checksum: "a43e82909d851e78dff7fa64edba12635e2e96206c1fcdca075eae02dd4c157e"
         ),
     ]
 )
