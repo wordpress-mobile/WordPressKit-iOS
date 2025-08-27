e93e79171450abcaf3ee13ebc7fd490e76db6773
Tony Li <tony.li@automattic.com>
2025-08-28 10:11:28 +1200

    Translate PostServiceRemoteREST.getLikesForPostID to Swift

--- Sources/WordPressKitObjC/PostServiceRemoteREST.m
+++ Sources/WordPressKitObjC/PostServiceRemoteREST.m
@@ -314,51 +314,6 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
            }];
 }
 
-- (void)getLikesForPostID:(NSNumber *)postID
-                    count:(NSNumber *)count
-                   before:(NSString *)before
-           excludeUserIDs:(NSArray<NSNumber *> *)excludeUserIDs
-                  success:(void (^)(NSArray<RemoteLikeUser *> * _Nonnull users, NSNumber *found))success
-                  failure:(void (^)(NSError * _Nullable))failure
-{
-    NSParameterAssert(postID);
-
-    NSString *path = [NSString stringWithFormat:@"sites/%@/posts/%@/likes", self.siteID, postID];
-    NSString *requestUrl = [self pathForEndpoint:path
-                                     withVersion:WordPressComRESTAPIVersion_1_2];
-    NSNumber *siteID = self.siteID;
-
-    // If no count provided, default to endpoint max.
-    if (count == 0) {
-        count = @90;
-    }
-
-    NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithDictionary:@{ @"number": count }];
-
-    if (before) {
-        parameters[@"before"] = before;
-    }
-
-    if (excludeUserIDs) {
-        parameters[@"exclude"] = excludeUserIDs;
-    }
-
-    [self.wordPressComRESTAPI get:requestUrl
-                       parameters:parameters
-                          success:^(id responseObject, NSHTTPURLResponse *httpResponse) {
-        if (success) {
-            NSArray *jsonUsers = responseObject[@"likes"] ?: @[];
-            NSArray<RemoteLikeUser *> *users = [self remoteUsersFromJSONArray:jsonUsers postID:postID siteID:siteID];
-            NSNumber *found = [responseObject numberForKey:@"found"] ?: @0;
-            success(users, found);
-        }
-    } failure:^(NSError *error, NSHTTPURLResponse *httpResponse) {
-        if (failure) {
-            failure(error);
-        }
-    }];
-}
-
 - (NSDictionary *)dictionaryWithRemoteOptions:(id <PostServiceRemoteOptions>)options
 {
     NSMutableDictionary *remoteParams = [NSMutableDictionary dictionary];
